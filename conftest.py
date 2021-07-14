5341683f51cdb10d6c6852a86eb6bd3b0f7c256b
@koro90 <karina@goaxiomatic.com>
2021-07-14 16:29:13 -0400

    add fixture

--- conftest.py
+++ conftest.py
@@ -1,8 +1,22 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import time
 
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default="es",
                  help="Choose language: en, ru, ...etc")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+    print("\nstart language for test..")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    print("\nquit browser..")
+    time.sleep(10)
+    browser.quit()
